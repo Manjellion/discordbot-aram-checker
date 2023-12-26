d40f1a5a2dfa60de7e754c2b60fc34dbc8149aeb
Manjil <rilleymanjil@hotmail.com>
2023-12-26 13:31:16 +0000

    Discord.JS file updated for checker storage

--- discord.js
+++ discord.js
@@ -25,10 +25,13 @@ client.on('ready', () => {
 
 // Collector with Button on Message
 client.on('messageCreate', async (message) => {
+  // Set the message interaction with the bot
   if (message.author.bot) return;
 
-  if (message.content !== 'Vote') return;
+  // Make sure the certain message is inputed to initate the voting
+  if (message.content !== '.vote') return;
 
+  // Build the buttons for the users to interact with the bot
   const confirmBtn = new ButtonBuilder()
     .setLabel('Confirm')
     .setStyle(ButtonStyle.Success)
@@ -39,47 +42,60 @@ client.on('messageCreate', async (message) => {
     .setStyle(ButtonStyle.Danger)
     .setCustomId('declined');
   
+  // Set a action row builder to comodedate and make the two seperate buttons into a singular components
   const buttonRow = new ActionRowBuilder().addComponents(confirmBtn, declineBtn);
 
+  // A message from the bot and the components it will carry, set up in async so make it await before it initiates into the variable
   const reply = await message.reply({ content: `Click on a button noob... YOU HAVE 1 MINUTE`, components: [buttonRow] });
 
+  // Filter in the user interaction
   const filter = (i) => i.user.id === message.author.id;
 
+  // Set up the Discord API collector, set up a collection of users who are interacting with the bot so we can collect their Global Name
   const collector = reply.createMessageComponentCollector({
     componentType: ComponentType.Button,
     filter,
     time: 10_000, // Change to 60_000 on launch (10_000) is being used to test in shortcase
-
   });
 
+  // Set up a temp array, this will reset in each time the bot is called
   const confirmed = [];
+  confirmed.slice(0, 11);
 
+  // Collection will start on an interaction from user and bot
   collector.on('collect', (interaction) => {
+    // Make sure the array hasn't reached max capacity
     if(confirmed.length !== 10) {
+      // Check if user has clicked on confirmed
       if(interaction.customId === 'confirmed') {
+        // Push the users name into the array
         confirmed.push(interaction.user.globalName);
+        // Let user know they have successfully joined
         interaction.reply(`${interaction.user.globalName} has joined, player count - ${confirmed.length} / 10`);
         return;
       }
     }
+    // If user declines
     if(interaction.customId === 'declined') {
       interaction.reply(`${interaction.user.globalName} is a noob`);
       return;
     }
   })
 
+  // When the timer runs out from the collection, we want to disable the buttons so that no one else can interact with the bot
   collector.on('end', () => {
     confirmBtn.setDisabled(true);
     declineBtn.setDisabled(true);
 
+    // Send a message to the channel the bot is in with the final list of confirmed players
     message.channel.send(`Players: ${confirmed.length} / 10 have confirmed`)
+    // Loop through the temporary array and display users who have confirmed
     for (var i = 0; i < confirmed.length; i++) {
       message.channel.send(confirmed[i]);
-      console.log(confirmed);
     }
 
     reply.edit({
-      content: 'You are out of time',
+      content: 'You are out of time noob',
       components: [buttonRow]
     })
   })
@@ -88,15 +104,6 @@ client.on('messageCreate', async (message) => {
 
 // Log In to our bot
 client.login(process.env.TOKEN);
-
-client.on('messageCreate', msg => {
-  // You can view the msg object here with console.log(msg)
-   if (msg.content === 'Hello') {
-     msg.reply(`Hello ${msg.author.username}`);
-   } else if (msg.content === 'Ping') {
-    msg.reply(`Pong ${msg.author.username}`);
-  }
-});
 
 // Set up new commands for the bot
 client.commands = new Collection();
@@ -118,7 +125,7 @@ for ( const folder of commandFolder ) {
     if ('data' in command && 'execute' in command) {
       client.commands.set(command.data.name, command);
     } else {
-      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
     }
   }
 }
